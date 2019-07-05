e28c6b4017c9258a6c83eba6e8503cf2aa97f656
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-06 02:39:27 +0500

    Create starter models

--- revoratebot/models.py
+++ revoratebot/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Dispatcher(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    telegram_user_id = models.IntegerField(blank=True, null=True)
+    token = models.CharField(max_length=100)
+    confirmed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+
+class Driver(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    telegram_user_id = models.IntegerField(blank=True, null=True)
+    token = models.CharField(max_length=100)
+    confirmed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+
+class Rating(models.Model):
+    from_id = models.IntegerField()
+    to_id = models.IntegerField()
+    value = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.CharField(max_length=500)
