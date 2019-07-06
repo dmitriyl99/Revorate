5bb48f9c8d279ec92e721c30024b8c12d196eff3
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-06 14:09:10 +0500

    String resources

---
+++ resources/strings.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Load strings from json
+# Russian language
+_strings_ru = json.loads(open(os.path.join(_basedir, 'strings_ru.json'), 'r', encoding='utf8').read())
+
+# Uzbek language
+_strings_uz = json.loads(open(os.path.join(_basedir, 'strings_uz.json'), 'r', encoding='utf8').read())
+
+# English language
+_strings_en = json.loads(open(os.path.join(_basedir, 'strings_en.json'), 'r', encoding='utf8').read())
+
+def get_string(key, language='ru'):
+    if language == 'ru':
+        return _strings_ru.get(key, 'no_string')
+    elif language == 'uz':
+        return _strings_uz.get(key, 'no_string')
+    elif language == 'en':
+        return _strings_en.get(key, 'no_string')
+    else:
+        raise Exception('Invalid language')
