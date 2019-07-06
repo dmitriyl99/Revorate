65df9fea1759ad2931a1a7ac4b270b24fe58cb76
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-06 14:09:24 +0500

    Bot settings

--- Revorate/settings.py
+++ Revorate/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Telegram Bot settings
+
+API_TOKEN = os.environ.get('API_TOKEN')
+WEBHOOK_HOST = os.environ.get('WEBHOOK_HOST')
+WEBHOOK_URL_BASE = 'https://%s/bot' % WEBHOOK_HOST
+WEBHOOK_URL_PATH = '%s/' % API_TOKEN
+
