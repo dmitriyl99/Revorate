1c4d854a7aefd4ae6785a49a45c66542d4f26196
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-07 16:29:46 +0500

    Documentation strings

--- core/managers/users.py
+++ core/managers/users.py
@@ -1,8 +1,13 @@
-from revoratebot.models import User
+from revoratebot.models import User, Department
 from typing import Optional
 
 
 def get_user_by_token(token: str) -> Optional[User]:
+    """
+    Get user by token
+    :param token: token
+    :return: User or None
+    """
     try:
         user = User.objects.filter(token=token)[0]
     except IndexError:
@@ -11,6 +16,11 @@ def get_user_by_token(token: str) -> Optional[User]:
 
 
 def get_user_by_telegram_id(user_id: int) -> Optional[User]:
+    """
+    Get user by Telegram ID
+    :param user_id: Telegram ID
+    :return: User or None
+    """
     try:
         user = User.objects.filter(telegram_user_id=user_id)[0]
     except IndexError:
@@ -19,6 +29,13 @@ def get_user_by_telegram_id(user_id: int) -> Optional[User]:
 
 
 def confirm_user(user: User, telegram_user_id):
+    """
+    Confirm user registration
+    :param user: User
+    :param telegram_user_id: User Telegram ID
+    :return: If registration confirmed return True else return False
+    """
+    # If user registration already confirmed prevent access
     if user.confirmed:
         return False
     user.confirmed = True
@@ -28,5 +45,11 @@ def confirm_user(user: User, telegram_user_id):
 
 
 def set_user_language(user: User, language):
+    """
+    Set user language for Telegram Bot
+    :param user: User
+    :param language: language code, like 'ru', 'en' and 'uz'
+    :return: void
+    """
     user.language = language
     user.save()
