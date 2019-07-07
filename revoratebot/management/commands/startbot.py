ec8cf49de98c23350be2e8f72be00794dd45034d
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-07 15:17:31 +0500

    Command for debug telegram bot

---
+++ revoratebot/management/commands/startbot.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from bot import telegram_bot
+import telebot
+import logging
+
+
+class Command(BaseCommand):
+    help = "Start the telegram bot without server"
+
+    def handle(self, *args, **options):
+        from bot import registration
+        telegram_bot.remove_webhook()
+        telebot.logger.setLevel(logging.DEBUG)
+        self.stdout.write(self.style.SUCCESS('Starting Telegram bot in polling mode...'))
+        telegram_bot.polling(none_stop=True)
+        self.stdout.write(self.style.SUCCESS('Telegram bot has been stopped'))
