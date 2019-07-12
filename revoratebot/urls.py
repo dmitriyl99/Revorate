1737db0b40a9a73f2752b5f522ae77614da5eed9
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-13 03:34:08 +0500

    Show company list and departments

--- revoratebot/urls.py
+++ revoratebot/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, companies
 
 urlpatterns = [
-    path('', index.IndexView.as_view(), name='admin_home')
+    path('', index.IndexView.as_view(), name='admin_home'),
+    path('companies/', companies.CompaniesListView.as_view(), name='admin_companies_list'),
+    path('companies/<int:pk>', companies.CompanyView.as_view(), name='admin_company')
 ]
---
+++ revoratebot/views/companies.py
@@ -0,0 +1,29 @@
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import ListView
+from django.views.generic.detail import SingleObjectMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
+from revoratebot.models import Company, Department, User
+
+
+class CompaniesListView(ListView):
+    template_name = 'admin/companies_list.html'
+    context_object_name = 'companies'
+    model = Company
+
+
+class CompanyView(ListView, SingleObjectMixin):
+    template_name = 'admin/company.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Company.objects.all())
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['company'] = self.object
+        return context
+
+    def get_queryset(self):
+        return self.object.department_set.all()
