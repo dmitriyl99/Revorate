2650198aa6508454c37198d01ca5488b4c26196b
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-17 01:37:09 +0500

    Create companies, departments, get department by id

--- core/managers/companies.py
+++ core/managers/companies.py
@@ -21,3 +21,24 @@ def get_company_by_id(company_id) -> Optional[Company]:
     except Company.DoesNotExist:
         return None
     return company
+
+
+def create_company(name: str) -> Company:
+    company = Company(name=name)
+    company.save()
+    create_default_company_departments(company)
+    return company
+
+
+def create_department_for_company(department_name: str, company: Company) -> Department:
+    department = Department(name=department_name, company=company)
+    department.save()
+    return department
+
+
+def get_department_by_id(department_id: int) -> Optional[Department]:
+    try:
+        department = Department.objects.get(pk=department_id)
+    except Department.DoesNotExist:
+        return None
+    return department
