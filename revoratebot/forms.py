4ba5446aee7891f23d706802cd225eb7c109238e
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-17 19:23:54 +0500

    Company and department is not required for managers

--- revoratebot/forms.py
+++ revoratebot/forms.py
@@ -4,6 +4,6 @@ from django import forms
 class CreateUserForm(forms.Form):
     name = forms.CharField(max_length=100)
     phone_number = forms.CharField(max_length=20)
-    company = forms.CharField()
-    department = forms.CharField()
+    company = forms.CharField(required=False)
+    department = forms.CharField(required=False)
     is_manager = forms.BooleanField(initial=False, required=False)
