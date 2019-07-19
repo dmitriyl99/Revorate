a81f72a7d1b07d0ba85f9950f32168c209bf1b1b
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-20 00:09:31 +0500

    Companies and departments in UserListView

--- revoratebot/views/users.py
+++ revoratebot/views/users.py
@@ -15,6 +15,12 @@ class UsersListView(LoginRequiredMixin, ListView):
     template_name = 'admin/users/users_list.html'
     context_object_name = 'users'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['companies'] = companies.get_all_companies()
+        context['departments'] = companies.get_all_departments()
+        return context
+
 
 class CreateUserView(LoginRequiredMixin, FormView):
     form_class = CreateUserForm
