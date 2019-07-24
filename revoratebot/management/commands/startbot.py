36d4fec86e46633176f8483612b1e93925d1099d
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-25 01:25:52 +0500

    Init scheduler

--- revoratebot/management/commands/startbot.py
+++ revoratebot/management/commands/startbot.py
@@ -9,8 +9,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         from bot import registration, settings, estimates, sos, managers
+        from core import scheduler
         telegram_bot.remove_webhook()
         telebot.logger.setLevel(logging.DEBUG)
         self.stdout.write(self.style.SUCCESS('Starting Telegram bot in polling mode...'))
+        scheduler.init()
         telegram_bot.polling(none_stop=True)
         self.stdout.write(self.style.SUCCESS('Telegram bot has been stopped'))
