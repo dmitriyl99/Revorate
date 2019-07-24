e4c61c0ecf28ff9075eef628ebea2d4f9b4bef98
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-24 14:16:52 +0500

    Company Rating View model

--- revoratebot/viewmodels.py
+++ revoratebot/viewmodels.py
@@ -1,5 +1,6 @@
 from revoratebot.models import Rating
 from core.managers import users
+from typing import List, Dict
 
 
 class RatingViewModel:
@@ -9,3 +10,10 @@ class RatingViewModel:
         self.value = rating.value
         self.created_at = rating.created_at
         self.comment = rating.comment
+
+
+class CompanyRatingViewModel:
+    def __init__(self, rating: Dict):
+        self.user = users.get_by_id(rating['to_id'])
+        self.avg_value = rating['avg_value']
+        self.count = rating['count']
