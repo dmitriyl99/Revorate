ec6a19de4e7675c4f3b01e8d723f5dff3178711e
dmitriyl99 <dmitriyl1899@gmail.com>
2019-07-31 02:39:58 +0500

    Add edit rating view

--- revoratebot/views/ratings.py
+++ revoratebot/views/ratings.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import UpdateView
 from core.managers import companies, ratings
 from revoratebot.viewmodels import RatingViewModel, CompanyRatingViewModel
 from revoratebot.models import Rating
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 class RatingsListView(LoginRequiredMixin, ListView):
@@ -38,5 +41,23 @@ class BotCompanyRatingView(TemplateView):
         context['confirmed_users_count'] = company_users_count[1]
         context['not_confirmed_users_count'] = company_users_count[2]
         return context
+
+
+class EditRatingView(LoginRequiredMixin, UpdateView):
+    model = Rating
+    template_name = 'admin/ratings/edit.html'
+    fields = ['comment']
+    success_url = reverse_lazy('admin_ratings')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        messages.success(self.request, "Комментарий рейтинга изменён!")
+        return result
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        rating = self.get_object()
+        view_model = RatingViewModel(rating)
+        context["rating"] = view_model
+        return context
     
